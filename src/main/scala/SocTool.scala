29aaa4b8fe2a779ae06cb37303cc1975effbfb22
Kevin Hartman <kevin@hart.mn>
2020-05-01 02:08:43 -0400

    Remove --action (it's always 'extract'), and rename --portable to --make-portable.

--- src/main/scala/SocTool.scala
+++ src/main/scala/SocTool.scala
@@ -17,6 +17,7 @@ import scala.util.{Success, Try}
 //  - Support port ID patching for more action types (var1 and var2)
 //  - Add external sound dependencies to listing.
 //  - Print dependencies for SOC format.
+//  - Add option to suppress entry info comments.
 
 object SocTool extends App {
 
@@ -39,12 +40,11 @@ object SocTool extends App {
   val sounds = toIdList(argValue(Set("--sound-ids")))
   val levels = toIdList(argValue(Set("--level-ids")))
 
-  val action = argValue(Set("--action", "-a"))
   val socFile = argValue(Set("--soc", "-s"))
 
   val fromOld = argFlag(Set("--from-old-srb2"))
   val toLua = argFlag(Set("--to-lua", "-l"))
-  val portable = argFlag(Set("--portable", "-p"))
+  val portable = argFlag(Set("--make-portable", "-p"))
   val freeslotPrefix = argValue(Set("--freeslot-prefix")).getOrElse("")
   val genFreeSlots = argFlag(Set("--freeslots", "-f"))
 
@@ -53,68 +53,64 @@ object SocTool extends App {
     file
   }
 
-  def doExtract(): Unit = {
-    val extracted = loadFile() match {
-      case Some(file) =>
-        val lines = file.getLines()
-        val script = SocScript(lines.toSeq)
+  val extracted = loadFile() match {
+    case Some(file) =>
+      val lines = file.getLines()
+      val script = SocScript(lines.toSeq)
 
-        if (Seq(things, states, sounds, levels).forall(_.isEmpty))
-          // Return all if no filters.
-          script
-        else {
-          val withThings = things.foldLeft(SocScript())((soc, id) => script.extractThing(id, soc))
-          val withStates = states.foldLeft(withThings)((soc, id) => script.extractState(id, soc))
-          val withSounds = sounds.foldLeft(withStates)((soc, id) => script.extractSound(id, soc))
-          val withLevels = levels.foldLeft(withSounds)((soc, id) => script.extractLevel(id, soc))
+      if (Seq(things, states, sounds, levels).forall(_.isEmpty))
+        // Return all if no filters.
+        script
+      else {
+        val withThings = things.foldLeft(SocScript())((soc, id) => script.extractThing(id, soc))
+        val withStates = states.foldLeft(withThings)((soc, id) => script.extractState(id, soc))
+        val withSounds = sounds.foldLeft(withStates)((soc, id) => script.extractSound(id, soc))
+        val withLevels = levels.foldLeft(withSounds)((soc, id) => script.extractLevel(id, soc))
 
-          withLevels
-        }
-      case None => error("SOC file not found")
-    }
-
-    def adjustHardcodedSlot(adjustment: Int => String)(id: String): Option[String] = {
-      Try(id.toInt) match {
-        case Success(0) => Some("0") // 0 is special case for null. Don't adjust!
-        case Success(numericId) => Some(adjustment(numericId))
-        case _ => None
+        withLevels
       }
-    }
-
-    def generateSlotName(hardcodedSlot: Int, maxLength: Int, minLength: Int = 0, radix: Int = 10) = {
-      val asBigInt: BigInt = hardcodedSlot
-      val radixEncodedId = asBigInt.toString(radix)
-
-      if (radixEncodedId.length > maxLength)
-        error("Hardcoded slot ID is too big to be auto-converted to freeslot name.")
-
-      val prefix = freeslotPrefix.take(maxLength - radixEncodedId.length)
-      val pad = "0" * (minLength - (prefix.length + radixEncodedId.length)) // Negative is ok!
-
-      (prefix + pad + radixEncodedId).toUpperCase
-    }
-
-    val ported = if (portable) MakePortable(
-      SlotRenameRules(
-        thingId = id => adjustHardcodedSlot(s => s"MT_${generateSlotName(s, 20)}")(id),
-        stateId = id => adjustHardcodedSlot(s => s"S_${generateSlotName(s, 20)}")(id),
-        soundId = id => adjustHardcodedSlot(s => s"sfx_${generateSlotName(s, 6).toLowerCase}")(id),
-        spriteId = id =>
-          // Note: sprite IDs get base-36 encoded since they're only allowed to be 4 chars long!
-          adjustHardcodedSlot(s => s"SPR_${generateSlotName(s, 4, minLength = 4, radix = 36)}")(id)
-      )
-    )(extracted) else extracted
-
-    val withDependencyInfo = BuildDependencyInfo(ported)
-
-    val print: SocScript => Unit = if (toLua)
-      PrintAsLua(PrinterConfig())
-    else
-      PrintAsSoc(PrinterConfig())
-
-    /* print extracted blocks to stdout */
-    print(withDependencyInfo)
+    case None => error("SOC file not found")
   }
 
-  doExtract()
+  def adjustHardcodedSlot(adjustment: Int => String)(id: String): Option[String] = {
+    Try(id.toInt) match {
+      case Success(0) => Some("0") // 0 is special case for null. Don't adjust!
+      case Success(numericId) => Some(adjustment(numericId))
+      case _ => None
+    }
+  }
+
+  def generateSlotName(hardcodedSlot: Int, maxLength: Int, minLength: Int = 0, radix: Int = 10) = {
+    val asBigInt: BigInt = hardcodedSlot
+    val radixEncodedId = asBigInt.toString(radix)
+
+    if (radixEncodedId.length > maxLength)
+      error("Hardcoded slot ID is too big to be auto-converted to freeslot name.")
+
+    val prefix = freeslotPrefix.take(maxLength - radixEncodedId.length)
+    val pad = "0" * (minLength - (prefix.length + radixEncodedId.length)) // Negative is ok!
+
+    (prefix + pad + radixEncodedId).toUpperCase
+  }
+
+  val ported = if (portable) MakePortable(
+    SlotRenameRules(
+      thingId = id => adjustHardcodedSlot(s => s"MT_${generateSlotName(s, 20)}")(id),
+      stateId = id => adjustHardcodedSlot(s => s"S_${generateSlotName(s, 20)}")(id),
+      soundId = id => adjustHardcodedSlot(s => s"sfx_${generateSlotName(s, 6).toLowerCase}")(id),
+      spriteId = id =>
+        // Note: sprite IDs get base-36 encoded since they're only allowed to be 4 chars long!
+        adjustHardcodedSlot(s => s"SPR_${generateSlotName(s, 4, minLength = 4, radix = 36)}")(id)
+    )
+  )(extracted) else extracted
+
+  val withDependencyInfo = BuildDependencyInfo(ported)
+
+  val print: SocScript => Unit = if (toLua)
+    PrintAsLua(PrinterConfig())
+  else
+    PrintAsSoc(PrinterConfig())
+
+  /* print extracted blocks to stdout */
+  print(withDependencyInfo)
 }
