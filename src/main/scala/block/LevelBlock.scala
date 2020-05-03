df0ff0f61b71541d86b272635137da5789b3c40b
Kevin Hartman <kevin@hart.mn>
2020-05-03 19:00:55 -0400

    Add missing properties and name variants for Level SOC.

--- src/main/scala/block/LevelBlock.scala
+++ src/main/scala/block/LevelBlock.scala
@@ -1,5 +1,6 @@
 package block
 
+import block.Line.{KeyIn, WellDefinedKey}
 import model.Level
 
 object LevelBlock extends Block[Level] {
@@ -10,21 +11,30 @@ object LevelBlock extends Block[Level] {
     with EqualsDelimiter
     with KeyExactly
   {
-    override val keyName: String = "LEVELNAME"
+    override val keyName: String = "Levelname"
+  }
+
+  object ActLine extends Line.Distinct[String]
+    with EqualsDelimiter
+    with KeyExactly
+  {
+    override val keyName: String = "Act"
   }
 
   object NextLevelLine extends Line.Distinct[String]
     with EqualsDelimiter
     with KeyExactly
   {
-    override val keyName: String = "NEXTLEVEL"
+    override val keyName: String = "NextLevel"
   }
 
   object MusicSlotLine extends Line.Distinct[String]
     with EqualsDelimiter
-    with KeyExactly
+    with KeyIn
+    with WellDefinedKey
   {
-    override val keyName: String = "MUSICSLOT"
+    override val keyName: String = "Music"
+    override val keys: Set[String] = Set("MusicSlot", "Music")
   }
 
   object WeatherLine extends Line.Distinct[String]
@@ -38,7 +48,21 @@ object LevelBlock extends Block[Level] {
     with EqualsDelimiter
     with KeyExactly
   {
-    override val keyName: String = "SKYNUM"
+    override val keyName: String = "SkyNum"
+  }
+
+  object SkyboxScaleLine extends Line.Distinct[String]
+    with EqualsDelimiter
+    with KeyExactly
+  {
+    override val keyName: String = "SkyboxScale"
+  }
+
+  object RecordAttackLine extends Line.Distinct[String]
+    with EqualsDelimiter
+    with KeyExactly
+  {
+    override val keyName: String = "RecordAttack"
   }
 
   object NoZoneLine extends Line.Distinct[String]
@@ -59,14 +83,28 @@ object LevelBlock extends Block[Level] {
     with EqualsDelimiter
     with KeyExactly
   {
-    override val keyName: String = "LEVELSELECT"
+    override val keyName: String = "LevelSelect"
   }
 
   object TypeOfLevelLine extends Line.Distinct[String]
     with EqualsDelimiter
     with KeyExactly
   {
-    override val keyName: String = "TYPEOFLEVEL"
+    override val keyName: String = "TypeOfLevel"
+  }
+
+  object SaveGameLine extends Line.Distinct[String]
+    with EqualsDelimiter
+    with KeyExactly
+  {
+    override val keyName: String = "SaveGame"
+  }
+
+  object FlickyListLine extends Line.Distinct[String]
+    with EqualsDelimiter
+    with KeyExactly
+  {
+    override val keyName: String = "FlickyList"
   }
 
   override def parseHeader: PartialFunction[String, Level] = {
@@ -77,29 +115,39 @@ object LevelBlock extends Block[Level] {
 
   override def parseProperty(level: Level): PartialFunction[String, Level] = {
     case LevelNameLine(name)          => level.copy(levelName = Some(name))
+    case ActLine(act)                 => level.copy(act = Some(act))
     case NextLevelLine(levelId)       => level.copy(nextLevel = Some(levelId))
     case MusicSlotLine(slot)          => level.copy(musicSlot = Some(slot))
     case WeatherLine(weather)         => level.copy(weather = Some(weather))
     case SkyNumLine(num)              => level.copy(skyNum = Some(num))
+    case SkyboxScaleLine(scale)       => level.copy(skyboxScale = Some(scale))
+    case RecordAttackLine(rec)        => level.copy(recordAttack = Some(rec))
+    case SaveGameLine(save)           => level.copy(saveGame = Some(save))
     case NoZoneLine(noZone)           => level.copy(noZone = Some(noZone))
     case NoSSMusicLine(noSSMusic)     => level.copy(noSSMusic = Some(noSSMusic))
     case LevelSelectLine(levelSelect) => level.copy(levelSelect = Some(levelSelect))
     case TypeOfLevelLine(typeOfLevel) => level.copy(typeOfLevel = Some(typeOfLevel))
+    case FlickyListLine(flickies)     => level.copy(flickyList = Some(flickies))
   }
 
   override def writeHeader(level: Level): String = HeaderLine(level.id)
 
   override def writeProperties(level: Level): Seq[String] = {
     Seq(
-      level.levelName  .map(LevelNameLine(_)),
-      level.nextLevel  .map(NextLevelLine(_)),
-      level.musicSlot  .map(MusicSlotLine(_)),
-      level.weather    .map(WeatherLine(_)),
-      level.skyNum     .map(SkyNumLine(_)),
-      level.noZone     .map(NoZoneLine(_)),
-      level.noSSMusic  .map(NoSSMusicLine(_)),
-      level.levelSelect.map(LevelSelectLine(_)),
-      level.typeOfLevel.map(TypeOfLevelLine(_))
+      level.levelName   .map(LevelNameLine(_)),
+      level.act         .map(ActLine(_)),
+      level.nextLevel   .map(NextLevelLine(_)),
+      level.musicSlot   .map(MusicSlotLine(_)),
+      level.weather     .map(WeatherLine(_)),
+      level.skyNum      .map(SkyNumLine(_)),
+      level.skyboxScale .map(SkyboxScaleLine(_)),
+      level.recordAttack.map(RecordAttackLine(_)),
+      level.saveGame    .map(SaveGameLine(_)),
+      level.noZone      .map(NoZoneLine(_)),
+      level.noSSMusic   .map(NoSSMusicLine(_)),
+      level.levelSelect .map(LevelSelectLine(_)),
+      level.typeOfLevel .map(TypeOfLevelLine(_)),
+      level.flickyList  .map(FlickyListLine(_))
     ).flatten
   }
 }
--- src/main/scala/model/Level.scala
+++ src/main/scala/model/Level.scala
@@ -3,12 +3,17 @@ package model
 case class Level(
   id: String,
   levelName: Option[String] = None,
+  act: Option[String] = None,
   nextLevel: Option[String] = None,
   musicSlot: Option[String] = None,
   weather: Option[String] = None,
   skyNum: Option[String] = None,
+  skyboxScale: Option[String] = None,
+  recordAttack: Option[String] = None,
+  saveGame: Option[String] = None,
   noZone: Option[String] = None,
   noSSMusic: Option[String] = None,
   levelSelect: Option[String] = None,
-  typeOfLevel: Option[String] = None
+  typeOfLevel: Option[String] = None,
+  flickyList: Option[String] = None
 )
